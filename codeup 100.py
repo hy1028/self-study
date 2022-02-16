c58e67e59b05298fd24b8cd0b8435d617380437f
hy1028 <97233967+hy1028@users.noreply.github.com>
2022-02-16 14:48:26 +0900

    Update codeup 100.py

---
+++ codeup 100.py
@@ -0,0 +1,116 @@
+# #6063
+# a, b = map(int, input().split(' '))
+# c = a if (a >= b) else b
+# print(c)
+
+# #6064
+# a, b, c = map(int, input().split(' '))
+# d = (a if a <= b else b) if (a if a <= b else b) < c else c
+# print(d)
+
+# #6065
+# a, b, c = map(int, input().split(' '))
+# numlist = [a, b, c]
+# for i in numlist:
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         pass
+
+# #6066
+# a, b, c = map(int, input().split(' '))
+# numlist = [a, b, c]
+# for i in numlist:
+#     if i % 2 == 0:
+#         print('even')
+#     else:
+#         print('odd')
+
+# #6067
+# a = int(input())
+# if a < 0:
+#     if a % 2 == 0:
+#         print('A')
+#     else:
+#         print('B')
+# elif a > 0:
+#     if a % 2 == 0:
+#         print('C')
+#     else:
+#         print('D')
+
+# #6068
+# t = int(input())
+# if t >= 90:
+#     print('A')
+# elif t >= 70:
+#     print('B')
+# elif t >= 40:
+#     print('C')
+# else:
+#     print('D')
+
+# #6069
+# t = input()
+# if t == 'A':
+#     print('best!!!')
+# elif t == 'B':
+#     print('good!!')
+# elif t == 'C':
+#     print('run!')
+# elif t == 'D':
+#     print('slowly~')
+# else:
+#     print('what?')
+
+# #6070
+# s = int(input())
+# if s >= 12:
+#     print('winter')
+# elif s >= 9:
+#     print('fall')
+# elif s >= 6:
+#     print('summer')
+# elif s >= 3:
+#     print('spring')
+# else:
+#     print('winter')
+
+
+# #6071
+# n = 1
+# while n != 0:
+#     n = int(input())
+#     if n !=0:
+#         print(n)
+#     else:
+#         break
+
+#6072
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
